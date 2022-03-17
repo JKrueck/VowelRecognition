ebde30dfecc9f7b951f9a60cceb082f02ca9face
Jonas Krück <jonas.kruecknline.de>
2022-03-17 21:15:52 +0100

    Trees, Random Forests, Bagging

--- main.r
+++ main.r
@@ -2,6 +2,12 @@
 ##Trying out several different methods and evaluating their performance
 library("OpenML")
 library(MASS)
+library(gam)
+library(tree)
+library(randomForest)
+library(class)
+
+
 setOMLConfig(apikey = "c1994bdb7ecb3c6f3c8f3b35f4b47f1f")
 data = getOMLDataSet(data.id=58L)$data
 train = data.frame()
@@ -27,3 +33,54 @@ qdaTestErr = 1-mean(qdaTestPred == test$Class)
 table(qdaTestPred,test$Class)
 #We encounter an even worse test error of 58.9%
 #From these two methods we can conclude that easier methods aren't of use at all
+
+#Trees
+train$Speaker_Number = NULL
+test$Speaker_Number = NULL
+tree=tree(Class~.-Train_or_Test,train)
+plot(tree)
+text(tree)
+tree.pred = predict(tree,test,type="class")
+table(tree.pred,test$Class)
+treeTestErr = 1-mean(tree.pred == test$Class)
+#Test Error of nearly 62%
+#try tree pruning
+set.seed(1337)
+cv.tree = cv.tree(tree,FUN=prune.misclass)
+par(mfrow=c(1,2))
+plot(cv.tree$size,cv.tree$dev,type="b")
+plot(cv.tree$k,cv.tree$dev,type="b")
+prune.tree = prune.misclass(tree,best=23)
+pruneTree.pred = predict(prune.tree,test,type="class")
+pruneTreeTestErr = 1-mean(pruneTree.pred == test$Class)
+#Test error reduced to ~60%
+
+#Random Forest
+randFor = randomForest(Class~.-Train_or_Test,data=train,importance=TRUE)
+randFor.pred = predict(randFor,test,type="class")
+randFor.TestErr = 1-mean(randFor.pred == test$Class)
+#Here we achieve our lowest test error yet 40,3% !
+
+#Bagging
+bag = randomForest(Class~.-Train_or_Test,data=train,mtry=11,importance=TRUE)
+bag.pred = predict(bag,test, type="class")
+bagTestErr= 1-mean(bag.pred == test$Class)
+#When we consider every predictor at every split we get a test error of 50,6 %
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
